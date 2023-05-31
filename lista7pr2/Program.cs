e79ce8830e2e65450fcd0a45862036d7c906be71
CJ3016846 <CJ3016846@LAB01-PC17>
2023-05-31 10:43:55 -0300

    Adicionar arquivos de projeto.

---
+++ lista7pr2/Program.cs
@@ -0,0 +1,629 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista7pr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("qual o programa que você deseja executar ? ");
+            a = int.Parse(Console.ReadLine());
+
+            switch (a)
+            {
+                case 1:
+
+                    int numero, preço, total = 0;
+
+                    while (true)
+                    {
+
+                        Console.WriteLine("qual a quantidade desse produto ");
+                        numero = int.Parse(Console.ReadLine());
+                        if (numero == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("qual o preço do produto: ");
+                        preço = int.Parse(Console.ReadLine());
+
+                        total = preço * numero + total;
+
+                        if (total == 0)
+                        {
+                            break;
+                        }
+
+
+
+
+
+                    }
+                    Console.WriteLine("o valor total vai igual a:" + total);
+
+
+                    break;
+
+                case 2:
+
+
+                    double vl, qq, vt, vm, tu, vmm;
+
+                    string nome, num, nmm;
+
+                    Console.WriteLine("digite o nome do produto (digite 'parar' para parar)");
+
+                    nome = Console.ReadLine();
+
+                    if (nome == "parar")
+
+                    {
+
+                        break;
+
+                    }
+
+                    Console.WriteLine("digite o valor do produto por unidade");
+
+                    vl = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("digite o a quantidade do produto");
+
+                    qq = double.Parse(Console.ReadLine());
+
+                    vt = qq * vl;
+
+                    vm = vt;
+
+                    num = nome;
+
+                    vmm = vt;
+
+                    nmm = nome;
+
+                    while (nome != "parar")
+
+                    {
+
+                        Console.WriteLine("digite o nome do produto (digite 'parar' para parar)");
+
+                        nome = Console.ReadLine();
+
+                        if (nome == "parar")
+
+                        {
+
+                            break;
+
+                        }
+
+                        Console.WriteLine("digite o valor do produto por unidade");
+
+                        vl = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("digite o a quantidade do produto");
+
+                        qq = double.Parse(Console.ReadLine());
+
+                        vt = qq * vl;
+
+                        if (vt > vm)
+
+                        {
+
+                            num = nome;
+
+                        }
+
+                        if (vt < vmm)
+
+                        {
+
+                            nmm = nome;
+
+                        }
+                        int k = 0;
+                        k++;
+                    }
+
+                    Console.WriteLine("o produto com maior investimento é: " + num + " com um investimento de R$" + vm + " e o de menor investimento é: " + nmm + " com um investimento de R$" + vmm);
+
+                    break;
+
+                case 3:
+
+                    double na, pda, pt;
+
+                    Console.WriteLine("quantas avaliações terão neste bimestre?");
+
+                    na = double.Parse(Console.ReadLine());
+
+                    pt = 0;
+                    int i = 0;
+                    while (i < na)
+
+                    {
+
+                        Console.WriteLine("qual o peso das avaliações (ex: 3)");
+
+                        pda = double.Parse(Console.ReadLine());
+
+                        pt = pt + pda;
+                        i++;
+
+                    }
+
+                    pt = pt * 10;
+
+                    if (pt > 100)
+
+                    {
+
+                        Console.WriteLine("\nOs pesos ultrapassam 100%");
+
+                        break;
+
+                    }
+
+                    if (pt == 100)
+
+                    {
+
+                        Console.WriteLine("\nOs pesos resultam em 100%");
+
+                        break;
+
+                    }
+
+                    if (pt > 100)
+
+                    {
+
+                        Console.WriteLine("\nOs pesos nao atingem 100%");
+
+                        break;
+
+                    }
+
+                    break;
+
+                case 4:
+
+                    string estado;
+
+                    Console.WriteLine("qual a sigla do estado");
+
+                    estado = Console.ReadLine();
+
+                    switch (estado)
+
+                    {
+
+                        case "AC":
+
+                            Console.WriteLine("o estado é: Acre");
+
+                            break;
+
+                        case "AL":
+
+                            Console.WriteLine("o estado é: Alagoas");
+
+                            break;
+
+                        case "AP":
+
+                            Console.WriteLine("o estado é: Amapá");
+
+                            break;
+
+                        case "AM":
+
+                            Console.WriteLine("o estado é: Amazonas");
+
+                            break;
+
+                        case "BA":
+
+                            Console.WriteLine("o estado é: Bahia");
+
+                            break;
+
+                        case "CE":
+
+                            Console.WriteLine("o estado é: Ceará");
+
+                            break;
+
+                        case "DF":
+
+                            Console.WriteLine("Distrito Federal não é um estado, mas sim uma uniudade autônoma");
+
+                            break;
+
+                        case "ES":
+
+                            Console.WriteLine("o estado é: Espírito Santo");
+
+                            break;
+
+                        case "GO":
+
+                            Console.WriteLine("o estado é: Goiás");
+
+                            break;
+
+                        case "MA":
+
+                            Console.WriteLine("o estado é: Maranhão");
+
+                            break;
+
+                        case "MT":
+
+                            Console.WriteLine("o estado é: Mato Grosso");
+
+                            break;
+
+                        case "MS":
+
+                            Console.WriteLine("o estado é: Mato Grosso do Sul");
+
+                            break;
+
+                        case "MG":
+
+                            Console.WriteLine("o estado é: Minas Gerais");
+
+                            break;
+
+                        case "PA":
+
+                            Console.WriteLine("o estado é: Pará");
+
+                            break;
+
+                        case "PB":
+
+                            Console.WriteLine("o estado é: Paraíba");
+
+                            break;
+
+                        case "PR":
+
+                            Console.WriteLine("o estado é: Paraná");
+
+                            break;
+
+                        case "PE":
+
+                            Console.WriteLine("o estado é: Pernambuco");
+
+                            break;
+
+                        case "PI":
+
+                            Console.WriteLine("o estado é: Piauí");
+
+                            break;
+
+                        case "RJ":
+
+                            Console.WriteLine("o estado é: Rio de Janeiro");
+
+                            break;
+
+                        case "RN":
+
+                            Console.WriteLine("o estado é: Rio Grande do Norte");
+
+                            break;
+
+                        case "RS":
+
+                            Console.WriteLine("o estado é: Rio Grande do Sul");
+
+                            break;
+
+                        case "RO":
+
+                            Console.WriteLine("o estado é: Rondônia");
+
+                            break;
+
+                        case "RR":
+
+                            Console.WriteLine("o estado é: Roraima");
+
+                            break;
+
+                        case "SC":
+
+                            Console.WriteLine("o estado é: Santa Catarina");
+
+                            break;
+
+                        case "SP":
+
+                            Console.WriteLine("o estado é: São Paulo");
+
+                            break;
+
+                        case "SE":
+
+                            Console.WriteLine("o estado é: Sergipe");
+
+                            break;
+
+                        case "TO":
+
+                            Console.WriteLine("o estado é: Tocantins");
+
+                            break;
+
+                        default:
+
+                            Console.WriteLine("nao existe estado com essa sigla");
+
+                            break;
+
+                    }
+
+                    break;
+
+                case 5:
+
+                    int a, b;
+
+                    Console.WriteLine("digite o mês");
+
+                    a = int.Parse(Console.ReadLine());
+
+                    switch (a)
+
+                    {
+
+                        case 1:
+
+                            Console.WriteLine("a estação correspondente a este mês é: verão");
+
+                            break;
+
+                        case 2:
+
+                            Console.WriteLine("a estação correspondente a este mês é: verão");
+
+                            break;
+
+                        case 3:
+
+                            Console.WriteLine("qual é o dia?");
+
+                            b = int.Parse(Console.ReadLine());
+
+                            if (b < 20 && b > 1)
+
+                                Console.WriteLine("a estação correspondente a este mês é: verão");
+
+                            if (b >= 20 && b < 31)
+
+                                Console.WriteLine("a estação correspondente a este mês é: outono");
+
+                            break;
+
+                        case 4:
+
+                            Console.WriteLine("a estação correspondente a este mês é: verão");
+
+                            break;
+
+                        case 5:
+
+                            Console.WriteLine("a estação correspondente a este mês é: verão");
+
+                            break;
+
+                        case 6:
+
+                            Console.WriteLine("qual é o dia?");
+
+                            b = int.Parse(Console.ReadLine());
+
+                            if ( b< 21 && b > 1)
+
+                                Console.WriteLine("a estação correspondente a este mês é: outono");
+
+                            if (b >= 21 && b < 30)
+
+                                Console.WriteLine("a estação correspondente a este mês é: inverno");
+
+                            break;
+
+                        case 7:
+
+                            Console.WriteLine("a estação correspondente a este mês é: inverno");
+
+                            break;
+
+                        case 8:
+
+                            Console.WriteLine("a estação correspondente a este mês é: inverno");
+
+                            break;
+
+                        case 9:
+
+                            Console.WriteLine("qual é o dia?");
+
+                            b = int.Parse(Console.ReadLine());
+
+                            if ( b< 23 && b > 1)
+
+                                Console.WriteLine("a estação correspondente a este mês é: inverno");
+
+                            if ( b>= 23 && b < 30)
+
+                                Console.WriteLine("a estação correspondente a este mês é: primavera");
+
+                            break;
+
+                        case 10:
+
+                            Console.WriteLine("a estação correspondente a este mês é: primavera");
+
+                            break;
+
+                        case 11:
+
+                            Console.WriteLine("a estação correspondente a este mês é: primavera");
+
+                            break;
+
+                        case 12:
+
+                            Console.WriteLine("qual é o dia?");
+
+                            b = int.Parse(Console.ReadLine());
+
+                            if ( b< 22 && b > 1)
+
+                                Console.WriteLine("a estação correspondente a este mês é: primavera");
+
+                            if ( b>= 22 && b < 30)
+
+                                Console.WriteLine("a estação correspondente a este mês é: verão");
+
+                            break;
+
+                        default:
+
+                            Console.WriteLine("o valor digitado é invalido");
+
+                            break;
+
+                    }
+
+                    break;
+
+                case 6:
+
+                    double altura, kg, imc;
+
+                    Console.WriteLine("Digite sua altura (em metros): ");
+
+                    altura = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite seu peso (em kilos): ");
+
+                    kg = double.Parse(Console.ReadLine());
+
+                    imc = kg / (altura * altura);
+
+                    if (imc < 18.5)
+
+                    {
+
+                        Console.WriteLine("você está abaixo do peso");
+
+                        break;
+
+                    }
+
+                    if (imc >= 18.5 && imc <= 24.9)
+
+                    {
+
+                        Console.WriteLine("você está no peso comum");
+
+                        break;
+
+                    }
+
+                    if (imc >= 25 && imc <= 29.9)
+
+                    {
+
+                        Console.WriteLine("você está acima do peso");
+
+                        break;
+
+                    }
+
+                    if (imc >= 30 && imc < 34.9)
+
+                    {
+
+                        Console.WriteLine("você está com obesidade grau 1");
+
+                        break;
+
+                    }
+
+                    if (imc >= 35 && imc < 39.9)
+
+                    {
+
+                        Console.WriteLine("você está com obesidade grau 2");
+
+                        break;
+
+                    }
+
+                    if (imc >= 40)
+
+                    {
+
+                        Console.WriteLine("você está com obesidade grau 3");
+
+                        break;
+
+                    }
+
+                    break;
+
+                case 7:
+
+                    int num;
+
+                    Console.WriteLine("digite um numero");
+
+                    num = int.Parse(Console.ReadLine());
+                    int b = 1;
+                    while (b < num)
+
+                    {
+                        b++;
+                        if (num % b == 0)
+
+                            Console.WriteLine(i);
+
+                        else
+
+                            i++;
+
+                    }
+
+                    break;
+
+                default:
+
+                    Console.WriteLine("Número inválido");
+
+                    break;
+
+            }
+
+        }
+
+    }
+
+}
+
+        
+    
+
